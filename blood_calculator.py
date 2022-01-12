72f35ddd490b079fc16e345c41b2e6bba7e2185b
cameron-murr <cameron.murr@duke.edu>
2022-01-12 16:26:15 -0500

    Wrote function to receive generic test input

---
+++ blood_calculator.py
@@ -0,0 +1,16 @@
+def interface():
+    print("Blood Test Analysis")
+    keep_running = True
+    while(keep_running):
+        print("Options")
+        print("9-Quit")
+        choice = input ("Enter your choice: ")
+        if choice == "9":
+            keep_running = False
+    return
+
+def accept_input(test_name):
+    entry = input("Enter the {} test result: ".format(test_name))
+    return int(entry)
+
+interface()
