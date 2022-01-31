27334f15ebfefe6e390f47baca719ef9662fcbfd
cameron-murr <cameron.murr@duke.edu>
2022-01-31 17:33:42 -0500

    developed function to return boolean if given point is on line created by two other points

--- on_line.py
+++ on_line.py
@@ -2,3 +2,13 @@ def point_on_line(tuple1, tuple2, x):
     slope = (tuple2[1] - tuple1[1]) / (tuple2[0] - tuple1[0])
     y = slope * (x - tuple1[0]) + tuple1[1]
     return y
+
+def is_point_in_line(tuple1, tuple2, tuple3):
+    slope = (tuple2[1] - tuple1[1]) / (tuple2[0] - tuple1[0])
+    y = slope * (tuple3[0] - tuple1[0]) + tuple1[1]
+
+    if tuple3[1] == y:
+        return True
+
+    else:
+        return False
